e930114cc561f17fa69427e3cf294127ff6331a9
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-24 11:43:03 -0500

    Using new component

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,76 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { gql } from 'apollo-boost';
-import { compose, graphql } from 'react-apollo'
+import { graphql } from 'react-apollo'
 import { Form, Dropdown, Button } from 'semantic-ui-react';
+import { AuthorDropdown } from './AuthorDropdown';
 
 
-const getAuthorsQuery = gql`
-{
-  authors{
-    name
-    id
+export const AddBook = () => {
+  const [name, setName] = useState();
+  const changeName = ({ target: { value } }) => setName(value);
+
+  const [genre, setGenre] = useState();
+  const changeGenre = ({ target: { value } }) => setGenre(value);
+
+  const [author, setAuthor] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
   }
-}
-`;
+
+  return (
+    <Form id="add-book" onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Field >
+          <label>Book Title</label>
+          <input type="text" onChange={changeName}></input>
+        </Form.Field>
+        <Form.Field >
+          <label>Genre</label>
+          <input type="text" onChange={changeGenre}></input>
+        </Form.Field>
+        <Form.Field >
+          <label>Author</label>
+          <AuthorDropdown
+            author={author}
+            setAuthor={setAuthor}
+          />
+        </Form.Field>
+      </Form.Group>
+
+      <Button type="submit">Send it!</Button>
 
 
-const authorOptions = (authors) => authors ? authors.map(a => {
-  return {
-    key: a.id,
-    text: a.name,
-    value: a.id
-  }
-}) : null;
-
-
-export const AddBook = graphql(getAuthorsQuery)
-  (({ data }) => {
-    const [name, setName] = useState();
-    const changeName = ({ target: { value } }) => setName(value);
-
-    const [genre, setGenre] = useState();
-    const changeGenre = ({ target: { value } }) => setGenre(value);
-
-    const [author, setAuthor] = useState();
-    const changeAuthor = (e, { value }) => setAuthor(value);
-
-
-    const [authors, setAuthors] = useState([]);
-    useEffect(() => {
-      setAuthors(data.authors);
-    }, [data.authors])
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(e);
-    }
-
-    return (
-      <Form id="add-book" onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Field >
-            <label>Book Title</label>
-            <input type="text" onChange={changeName}></input>
-          </Form.Field>
-          <Form.Field >
-            <label>Genre</label>
-            <input type="text" onChange={changeGenre}></input>
-          </Form.Field>
-          <Form.Field >
-            <label>Author</label>
-            <Dropdown
-              placeholder="Select author"
-              selection
-              options={authorOptions(authors)}
-              onChange={changeAuthor}
-            />
-          </Form.Field>
-        </Form.Group>
-
-        <Button type="submit">Send it!</Button>
-
-
-      </Form>
-    )
-  });
+    </Form>
+  )
+};
 
